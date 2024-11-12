85470095e35663b96b88f5646ecd4be69a8004c9
gcoutu <gc8395@gmail.com>
2024-11-12 12:41:40 -0500

    Exercice Angular

---
+++ related_files/README.md
@@ -0,0 +1,10 @@
+# angular-e04 &mdash; Utilisation de composants Material Design
+[README](../README.md) &mdash; Angular
+
+## Commandes Angular à exécuter pour générer les composants de l'exercice
+```sh
+ng add @angular/material
+```
+
+Thème Material Design sélectionné :
+https://material.angular.io/?theme=indigo-pink
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatGridListModule, MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-e04';
+}
