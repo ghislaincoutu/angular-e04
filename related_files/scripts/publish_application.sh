d1d4c6d1eeb8fc1ae497870d2b3b0b2d7bc68657
gcoutu <gc8395@gmail.com>
2025-01-08 16:39:54 -0500

    Exercice Angular

--- related_files/scripts/publish_application.sh
+++ related_files/scripts/publish_application.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+
 function apply_pause() {
   read -p "$*"
 }
+
 echo "Publication de l'applications sur le serveur Web local"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 # Source (so)
-so=/home/Exercises/angular-e04/dist/angular-e04
+so=/home/Exercises/angular-e04/dist/angular-e04/browser
 # Destination (de)
 de=/var/www/html/d002/angular-e04
-rm -r $de/browser
-rm -r $de/server
-rm $de/*.*
-cp -R $so/browser $de/browser
-cp -R $so/server $de/server
+
+rm $de/favicon.ico
+rm $de/index.html
+rm $de/main*.js
+rm $de/polyfills*.js
+rm $de/styles*.css
 cp $so/*.* $de/
-sed -i 's/<base href="\/">/<base href="\/d002\/angular-e04\/browser\/">/g' $de/browser/index.html
+sed -i 's/<base href="\/">/<base href="\/d002\/angular-e04\/">/g' $de/index.html
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatGridListModule, MatSlideToggleModule],
+  imports: [CommonModule, MatGridListModule, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
