dc88a46eb12bfe41704f554a26026abe0b26baf2
gcoutu <gc8395@gmail.com>
2026-05-11 04:43:01 -0400

    Exercice Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -6,7 +7,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatGridListModule, MatSlideToggleModule],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatGridListModule, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
